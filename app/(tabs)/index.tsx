1d083b94cb3657522f06ac304b3da9debf3b223c
Ayush Sharma <sharayush1441@gmail.com>
2025-11-02 01:54:06 +0530

    new update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,101 +1,118 @@
+import { useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+
+import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
+import useFetch from "@/services/usefetch";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import TrendingCard from "@/components/TrendingCard";
-import { icons } from "@/constants/icons";
-import { images } from '@/constants/images';
-import { fetchMovies } from "@/services/api";
-import { getTrendingMovies } from "@/services/appwrite";
-import useFetch from '@/services/usefetch';
-import { useRouter } from "expo-router";
-import { ActivityIndicator, FlatList, Image, ScrollView, Text, View, } from "react-native";
-import '../globals.css';
 
-export default function Index() {
+const Index = () => {
   const router = useRouter();
 
   const {
     data: trendingMovies,
     loading: trendingLoading,
-    error: trendingError
-  } = useFetch(getTrendingMovies)
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
-    data:movies,
+    data: movies,
     loading: moviesLoading,
-    error: moviesError
-  } = useFetch(() => fetchMovies({query: ''}))
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
   return (
-    <View className='flex-1 bg-primary'>
-      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
-      <ScrollView 
-      className='flex-1 px-5' 
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{
-        minHeight: '100%', paddingBottom:10
-      }}
-      >
-        <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto'/>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
 
-        {moviesLoading  || trendingLoading ? (
-          <ActivityIndicator 
-          size='large'
-          color='#0000ff'
-          className='mt-10 self-center'
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading || trendingLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
         ) : moviesError || trendingError ? (
-          <Text>Error : {moviesError?.message || trendingError?.message}</Text>
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
-          <View className="flex-1 mt-5 bg-secondary">
-            <SearchBar 
-              placeholder="Search for movies or TV shows"
-              onPress={() => router.push('/Search')}
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/Search");
+              }}
+              placeholder="Search for a movie"
             />
+
             {trendingMovies && (
-              <View className='mt-10'>
-                <Text className='text-lg text-white font-bold mt-5 mb-3'>Trending Movies</Text>
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
               </View>
             )}
-            <>
-              
 
-              <FlatList 
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                ItemSeparatorComponent={() => <View className='w-4'/>}
-                data={trendingMovies} 
-                renderItem={({item, index}) => (
-                  <TrendingCard movie={item} index={index} />
-                )}
-                className='mb-4 mt-3'
-                keyExtractor={(item) => item.movie_id.toString()}
-              />
-              <Text className='text-lg text-white font-bold mt-5 mb-3'>Latest Movies</Text>
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
 
               <FlatList
-              data={movies}
-              renderItem={({item}) => (
-                <MovieCard 
-                  {...item}
-                />
-              )}
-              keyExtractor={(item) => item.id}
-              numColumns={3}
-              columnWrapperStyle={{
-                justifyContent:'flex-start',
-                gap:20,
-                paddingRight: 5,
-                marginBottom: 10
-              }}
-              className='mt-2 pb-32'
-              scrollEnabled={false}
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
               />
             </>
           </View>
         )}
-
-        
       </ScrollView>
     </View>
   );
-}
+};
+
+export default Index;
