80de83e9c73df15a71984d1a649860f2a527947b
Ayush Sharma <sharayush1441@gmail.com>
2025-11-02 01:16:01 +0530

    added trending

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen 
-    name="(tabs)"
-    options={{headerShown:false}}
-    />
-    <Stack.Screen 
-    name="movies/[id]"
-    options={{headerShown:false}}
-    />
-  </Stack>;
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  )
 }
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -45,3 +45,18 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     // if a document is found   increment the searchCount field
     // if no document is found create a new document in AppWrite database with searchCount = 1
 }
+
+export const getTrendingMovies = async () : Promise<TrendingMovie[] | undefined> => {
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ]);
+
+        return result.documents as unknown as TrendingMovie[];
+    }
+    catch(error){
+        console.log(error);
+        return undefined;
+    }
+}
